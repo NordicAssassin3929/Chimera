8d2184fe32d611f283c748b986986b0037aa88d1
nusic <nxu6202@rit.edu>
2020-10-25 09:49:15 +0100

    successfully addied product to products array of cart model

--- models/cart.js
+++ models/cart.js
@@ -5,35 +5,44 @@ module.exports = class Cart {
 
     static save(product) {
 
-        let cartModel = new CartModel();
-
         console.log('Product: ' + product)
         if (cart == null) {
             cart = {products: [], totalPrice: 0}
         }
 
         // if(userId == '5f940f8876ad3e073a2e1e8b') {
-        console.log('Cart already exists ')
         // 5f940f8876ad3e073a2e1e8b
-        let done = function(err, result) {} // this runs after the mongoose operation }
 
-        cartModel.updateOne(
+        let productObj = {
+            'title': product.title,
+            'price': product.price,
+            'amount': product.amount
+        };
+
+        let done = function (err, result) {
+        } // this runs after the mongoose operation }
+
+        CartModel.update(
             {
                 userId: '5f940f8876ad3e073a2e1e8b'
             },
             {
                 $push: {
-                    products:
-                        {
-                            'title': product.title,
-                            'price': product.price,
-                            'amount': product.amount
-                        },
+                    products: productObj
+                },
+                $set: {
                     totalPrice: product.amount * product.price
                 }
             },
-            done()
-            );
+            {upsert: true, new: true},
+            function (error, success) {
+                if (error) {
+                    console.log(error)
+                } else {
+                    console.log(success)
+                }
+            }
+        );
         // }
         // else { // doesn't exists
         // console.log('Cart already exists ')
