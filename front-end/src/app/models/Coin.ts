2f7ee268cdaab420a79466ba6a30f94c36cc0b9f
Natan Usic <natanu1994@gmail.com>
2021-01-01 21:04:17 +0100

    fixed icons, store validation, fixed delete

--- front-end/src/app/models/Coin.ts
+++ front-end/src/app/models/Coin.ts
@@ -2,7 +2,5 @@ export interface Coin {
   title: string
   price: number
   amount: number
-  imageURL: string
-  description: string,
   userId: string
 }
--- front-end/src/app/store/store.component.ts
+++ front-end/src/app/store/store.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {ApiService} from "../services/api.service";
-import {Coin} from "../models/Coin";
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from "../services/api.service";
+import { Coin } from "../models/Coin";
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-store',
@@ -11,12 +12,16 @@ import { Router } from '@angular/router';
 export class StoreComponent implements OnInit {
   coins: Coin[] = [];
   modalCoins: [] = [];
+  addToCartForm: FormGroup;
+  coin: Coin
 
   constructor(private apiService: ApiService,
-              private router: Router) {
+    private router: Router,
+    private fb: FormBuilder) {
+      this.initializeForm()
   }
 
-  async waitForThis(){
+  async waitForThis() {
     this.apiService.getCoins()
       .subscribe((data: any) => {
         console.log(data)
@@ -33,24 +38,39 @@ export class StoreComponent implements OnInit {
     await this.waitForThis().then(r => console.log(r))
   }
 
+  initializeForm() {
+    this.addToCartForm = this.fb.group({
+      amount: ['', [Validators.required, 
+        Validators.min(1),
+        Validators.pattern(/^[1-9]\d*$/)]]
+    })
+  }
+
   onSubmit(coin: Coin) {
-    console.log(coin);
+    this.coin = this.addToCartForm.value;
+    console.log(this.coin);
+    // this.coin.title = 'TEST'
+    // this.coin.price = 50
     const userId = localStorage.getItem('user_id');
 
     const newCoin = {
+      // title: this.coin.title,
+      // price: this.coin.price,
       title: coin.title,
       price: coin.price,
-      amount: coin.amount,
-      imageURL: coin.imageURL,
-      description: coin.description,
+      amount: this.coin.amount,
       userId: userId
     }
+
+    console.log('Status: ' + this.addToCartForm.status)
     
+    if (this.addToCartForm.valid) {
     this.apiService.buyCoin(newCoin)
-    .subscribe(
-      (data: Coin) => {
-        console.log(data);
-      }
-    )
+      .subscribe(
+        (data: Coin) => {
+          console.log(data);
+        }
+      )
+    }
   }
 }
